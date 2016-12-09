53f13636c398786d6c07515f9bb758ccf3be9bca
Jimy Ccatamayo <jimyccatamayo@gmail.com>
2016-12-09 17:04:15 +0000

    OK

--- lib/tstrangulo.rb
+++ lib/tstrangulo.rb
@@ -1,22 +1,23 @@
 class Tstrangulo
 	
 	def initialize
-	    @palabra = "LAMPARA"
+	    @palabra = "I G U A N O D O N T E "
 	    @palabraEspacios = ""
 	    @numLetras = 0
+	    @palabraPista = ""
 	end
-	
-	@palabra = "LAMPARA"
 	
 	PALABRAS_PLANTAS = {"PLA1"=>"orquidia","PLA2"=>"PLA3"}
 	
-	PALABRAS = "IGUANODONTE"
+	PALABRAS = "I G U A N O D O N T E "
 
 	def mostrarEspacios
-	    @numLetras = @palabra.length
+		tempo = @palabra
+		tempo = tempo.split " "
+		tempo = tempo.join ""
+	    @numLetras = tempo.length
 	    for i in 0..@numLetras-1
 	            @palabraEspacios = @palabraEspacios << "_ "
-
 	    end
 	end
 	
@@ -25,6 +26,21 @@ class Tstrangulo
 	end 
 	
 	def letraExisteEnPalabra(letra,palabra)
+		palabra = @palabra
 		palabra.include? letra
 	end
+	
+	def ponerLetraEnPalabra(letra,palabraPista)
+		ocurrencias = @palabra.count(letra)
+		val = 0
+		(1..ocurrencias).each do |n|
+    		indice = @palabra[val..@palabra.length].index(letra)
+			replacement = '...'
+			palabraPista[indice+val] = letra
+			val=indice+1
+		end
+		puts palabraPista
+		return palabraPista
+	end
 end
+
--- spec/tstrangulo_spec.rb
+++ spec/tstrangulo_spec.rb
@@ -4,11 +4,17 @@ describe Tstrangulo do
 	it "Se deben mostrar 7 espacios en guion bajo" do
 		tstrangulo = Tstrangulo.new
 		tstrangulo.mostrarEspacios
-		expect(tstrangulo.verPalabra).to eq "_ _ _ _ _ _ _ "
+		expect(tstrangulo.verPalabra).to eq "_ _ _ _ _ _ _ _ _ _ _ "
     end
 
 	it "Letra existe en palabra" do 
 		stran = Tstrangulo.new
-		expect(stran.letraExisteEnPalabra("Q","IGUANODONTE")).to eq true
+		expect(stran.letraExisteEnPalabra("U","I G U A N O D O N T E ")).to eq true
+	end
+	
+	it "Ingresa la A para descubrir la palabra" do
+	    stran = Tstrangulo.new
+		expect(stran.ponerLetraEnPalabra("A","_ _ _ _ _ _ _ _ _ _ _ ")).to eq "_ _ _ A _ _ _ _ _ _ _ "
+		expect(stran.ponerLetraEnPalabra("O",stran.ponerLetraEnPalabra("A","_ _ _ _ _ _ _ _ _ _ _ "))).to eq "_ _ _ A _ O _ O _ _ _ "
 	end
 end
