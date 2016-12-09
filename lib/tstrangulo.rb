56ff8ba0f3586e0d9a60ca9a2a163bea7bd97eb5
Jimy Ccatamayo <jimyccatamayo@gmail.com>
2016-12-09 19:22:36 +0000

    OK

--- lib/tstrangulo.rb
+++ lib/tstrangulo.rb
@@ -1,15 +1,21 @@
 class Tstrangulo
 	
-	def initialize
-	    @palabra = "I G U A N O D O N T E "
+	def initialize(aleatorio = rand(0...PALABRAS.length))
+	    @palabra = PALABRAS[aleatorio]
 	    @palabraEspacios = ""
 	    @numLetras = 0
 	    @palabraPista = ""
+	    @listaLetrasErradas=""
+	    
+	    #############################
+	    
+	    @numIntentos = 0
+	    @numFallos = 0
 	end
 	
-	PALABRAS_PLANTAS = {"PLA1"=>"orquidia","PLA2"=>"PLA3"}
-	
-	PALABRAS = "I G U A N O D O N T E "
+	MAX_FALLOS = 5
+	PALABRAS = ["I G U A N O D O N T E ", "S A R C O I D O S I S ", "M E N I N G O E N C E F A L I T I S ", "A N K Y L O S A U R U S ", "T R I P A N O S O M I A S I S ", "P L U M B A G O ", "V I B O R E R A ", "C A L C E O L A R I A "]
+	DEFINICIONES = ["Def 1", "Def 2", "Def 3", "Def 4", "Def 5", "Def 6", "Def 7", "Def 8"]
 
 	def mostrarEspacios
 		tempo = @palabra
@@ -25,12 +31,19 @@ class Tstrangulo
 	    @palabraEspacios
 	end 
 	
-	def letraExisteEnPalabra(letra,palabra)
+	def letraExisteEnPalabra(letra)
 		palabra = @palabra
 		palabra.include? letra
 	end
 	
 	def ponerLetraEnPalabra(letra)
+		if letraExisteEnPalabra(letra)
+			@numIntentos+=1
+		else
+			@numFallos+=1
+			@listaLetrasErradas = @listaLetrasErradas + ", " + letra
+		end
+			
 		ocurrencias = @palabra.count(letra)
 		val = 0
 		(1..ocurrencias).each do |n|
@@ -40,6 +53,11 @@ class Tstrangulo
 			val=indice+1
 		end
 		return @palabraEspacios
+	end
+	
+	def verEstadoJuego
+		restante = (MAX_FALLOS - @numFallos)
+		return "Te quedan #{restante} intentos"
 	end
 end
 
--- spec/tstrangulo_spec.rb
+++ spec/tstrangulo_spec.rb
@@ -2,20 +2,36 @@ require './lib/tstrangulo.rb'
 
 describe Tstrangulo do
 	it "Se deben mostrar 7 espacios en guion bajo" do
-		tstrangulo = Tstrangulo.new
+		tstrangulo = Tstrangulo.new 0
 		tstrangulo.mostrarEspacios
 		expect(tstrangulo.verPalabra).to eq "_ _ _ _ _ _ _ _ _ _ _ "
     end
 
 	it "Letra existe en palabra" do 
-		stran = Tstrangulo.new
-		expect(stran.letraExisteEnPalabra("U","I G U A N O D O N T E ")).to eq true
+		stran = Tstrangulo.new 0
+		expect(stran.letraExisteEnPalabra("A")).to eq true
 	end
 	
 	it "Ingresa la A para descubrir la palabra" do
-	    stran = Tstrangulo.new
+	    stran = Tstrangulo.new 0
 	    stran.mostrarEspacios
 		expect(stran.ponerLetraEnPalabra("A")).to eq "_ _ _ A _ _ _ _ _ _ _ "
 		expect(stran.ponerLetraEnPalabra("O")).to eq "_ _ _ A _ O _ O _ _ _ "
+	end
+	
+    it "Ingresa la letra A y O que son correctas, debe quedar 5 intentos" do
+	    stran = Tstrangulo.new 0
+	    stran.mostrarEspacios
+		stran.ponerLetraEnPalabra("A")
+		stran.ponerLetraEnPalabra("O")
+		expect(stran.verEstadoJuego).to eq "Te quedan 5 intentos"
+	end
+	
+	it "Ingresa la letra R y J que son incorrectas, debe quedar 3 intentos" do
+	    stran = Tstrangulo.new 0
+	    stran.mostrarEspacios
+		stran.ponerLetraEnPalabra("R")
+		stran.ponerLetraEnPalabra("J")
+		expect(stran.verEstadoJuego).to eq "Te quedan 3 intentos"
 	end
 end
